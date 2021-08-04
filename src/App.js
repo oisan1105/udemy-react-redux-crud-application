2b0571c3246b5af83c964b1f50dc7be2ba768eac
BuildTools <unconfigured@null.spigotmc.org>
2021-08-04 21:32:09 +0900

    Co-authored-by: wetti0816 <wetti0816@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-
+import { useState } from "react";
 
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname",  },
+
+    
+  ]
   return (
 <div>
-  <label htmlFor="bar">bar</label>
-  <input type="test" onChange={() => {console.log("i am clicked.")}}/>;
+  {
+    profiles.map((profile, index) => {
+       return <User name={profile.name} age={profile.age} key={index}/>
+    })
+  }
 </div>
   )
+}
+
+// function App() {
+//   const [state, setstate] = useState(false)
+//   return (
+//   <div>
+//     <button onClick={() => setstate(!state)}></button>
+//     <p>Text:{state ? "A":"B"}</p>
+//   </div>
+//   )
+// }
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 
