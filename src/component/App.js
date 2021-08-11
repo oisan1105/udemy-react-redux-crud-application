432f21e16c7f78cebf57b80f2424d4dd10f4be95
BuildTools <unconfigured@null.spigotmc.org>
2021-08-11 21:10:32 +0900

    implement componet with connect function

--- src/component/App.js
+++ src/component/App.js
@@ -1,35 +1,32 @@
-import React, { Component,useState }from "react";
+import React, { Component,useState } from "react";
+import { connect } from 'react-redux'
 
+import { increment, decrement } from '../actions'
 
-const App = () =>(<Counter></Counter>)
-
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  handlePlusButton = () => {
-    const currentCount = this.state.count
-    this.setState({ count: currentCount + 1 })
-  }
-
-  handleMinusButton = () => {
-    const currentCount = this.state.count
-    this.setState({ count: currentCount - 1 })
-  }
-
+class App extends Component {
   render() {
+    const props = this.props
+
     return(
       <React.Fragment>
-        <div>count: { this.state.count } </div>)
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value } </div>)
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
+
+const mapStateToProps = state => ({ value: state.ount.value })
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+
+
 
 
 // const CounterNew = () => {
@@ -45,4 +42,4 @@ class Counter extends Component {
 //   )
 // }
 
-export default App;
+
